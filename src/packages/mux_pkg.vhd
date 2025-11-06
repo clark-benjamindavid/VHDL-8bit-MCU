9004939693fac0adcb66e672da982fac794993b5
BenClark - Laptop <clark.benjamindavid@gmail.com>
2025-11-06 16:24:41 +1300

    added package for 2:1, 4:1, 8:1 mux

---
+++ src/packages/mux_pkg.vhd
@@ -0,0 +1,28 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.all;
+
+package mux_pkg is
+    component mux_2to1 is
+        port (
+            din : in std_logic_vector(1 downto 0); 
+            sel : in std_logic;
+            dout : out std_logic
+        );
+    end component mux_2to1;
+
+    component mux_4to1 is
+        port (
+            din : in std_logic_vector(3 downto 0);
+            sel : in std_logic_vector(1 downto 0);
+            dout : out std_logic
+        );
+    end component mux_4to1;
+
+    component mux_8to1 is
+        port (
+            din : in std_logic_vector(7 downto 0);
+            sel : in std_logic_vector (2 downto 0);
+            dout : out std_logic
+        );
+    end component mux_8to1;
+end package mux_pkg;
